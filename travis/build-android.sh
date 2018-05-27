f228a2786a6434285252be2ae8cfdff84724cb84
Nutan <nutan.nivate@gmail.com>
2018-05-27 13:14:19 +0530

    Added gradle

--- travis/build-android.sh
+++ travis/build-android.sh
@@ -7,7 +7,9 @@ cordova platform add android --nofetch
 
 if [[ "$TRAVIS_BRANCH" == "develop" ]]
 then
+    echo "building apk for dev"
     ionic cordova build android
 else
+    echo "building apk from prod"
     ionic cordova build android --prod --release
 fi
